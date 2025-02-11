c70d8304b676c615a7ae362357b1c556565eaf48
rn1xon <157616352+rn1xon@users.noreply.github.com>
2025-02-10 21:57:49 -0500

    Add files via upload

---
+++ Symptoms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC365_Project1.Models
+{
+    internal class Symptoms
+    {
+        public int VAERS_ID { get; set; }
+        public string SYMPTOM1 { get; set; }
+        public string SYMPTOMVERSION1 { get; set; }
+        public string SYMPTOM2 { get; set; }
+        public string SYMPTOMVERSION2 { get; set; }
+        public string SYMPTOM3 { get; set; }
+        public string SYMPTOMVERSION3 { get; set; }
+        public string SYMPTOM4 { get; set; }
+        public string SYMPTOMVERSION4 { get; set; }
+        public string SYMPTOM5 { get; set; }
+        public string SYMPTOMVERSION5 { get; set; }
+    }
+}
---
+++ VaxInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC365_Project1.Models
+{
+    internal class VaxInfo
+    {
+        public int VAERS_ID { get; set; }
+        public string VAX_TYPE { get; set; }
+        public string VAX_MANU { get; set; }
+        public string VAX_LOT { get; set; }
+        public string VAX_DOSE_SERIES { get; set; }
+        public string VAX_ROUTE { get; set; }
+        public string VAX_SITE { get; set; }
+        public string VAX_NAME { get; set; }
+
+    }
+}
