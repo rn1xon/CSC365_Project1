99d99ba1516fbbebc7e09baee91b20f47a9d50a4
rn1xon <157616352+rn1xon@users.noreply.github.com>
2025-02-24 01:06:10 -0500

    Update Symptoms.cs

--- Symptoms.cs
+++ Symptoms.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +9,15 @@ namespace CSC365_Project1.Models
     internal class Symptoms
     {
         public int VAERS_ID { get; set; }
-        public string SYMPTOM1 { get; set; }
-        public string SYMPTOMVERSION1 { get; set; }
-        public string SYMPTOM2 { get; set; }
-        public string SYMPTOMVERSION2 { get; set; }
-        public string SYMPTOM3 { get; set; }
-        public string SYMPTOMVERSION3 { get; set; }
-        public string SYMPTOM4 { get; set; }
-        public string SYMPTOMVERSION4 { get; set; }
-        public string SYMPTOM5 { get; set; }
-        public string SYMPTOMVERSION5 { get; set; }
+        public string SYMPTOM1 { get; set; } = String.Empty;
+        public string SYMPTOMVERSION1 { get; set; } = String.Empty;
+        public string SYMPTOM2 { get; set; } = String.Empty;
+        public string SYMPTOMVERSION2 { get; set; } = String.Empty;
+        public string SYMPTOM3 { get; set; } = String.Empty;
+        public string SYMPTOMVERSION3 { get; set; } = String.Empty;
+        public string SYMPTOM4 { get; set; } = String.Empty;
+        public string SYMPTOMVERSION4 { get; set; } = String.Empty;
+        public string SYMPTOM5 { get; set; } = String.Empty;
+        public string SYMPTOMVERSION5 { get; set; } = String.Empty;
     }
 }
