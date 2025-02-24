f35df0e04e5c99aaa676cc8869687b40d9e25b20
rn1xon <157616352+rn1xon@users.noreply.github.com>
2025-02-24 01:08:44 -0500

    Create Task3Grouping.cs

---
+++ Task3Grouping.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace CSC365_Project1.Models
+{
+    public class Task3Grouping
+    {
+        public string Bucket {  get; set; }
+        public string SEX {  get; set; }
+        public string VAX_NAME { get; set; }
+        public string SYMPTOM {  get; set; }
+        public int DeathCount { get; set; }
+        public int SortOrder { get; set; }
+    }
+}
